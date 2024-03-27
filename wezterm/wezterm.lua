7b35b3c4232b43c1e5f421dacc3a209486bbef1c
braidonw <braidon@braidonwhatley.com>
2024-03-27 14:50:04 +1100

    wip

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,7 +5,10 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("Iosevka Term", { stretch = "Expanded", weight = "Medium" })
-config.font_size = 15
-config.color_scheme = "Gruvbox dark, hard (base16)"
+config.font_size = 14
+config.color_scheme_dirs = { "~/.config/wezterm/colors" }
+config.color_scheme = "Bamboo"
+
+config.tab_bar_at_bottom = false
 
 return config
