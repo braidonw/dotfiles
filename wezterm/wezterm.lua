4ed422371238ff7adc9b7ad26bb1d1bf314939a5
braidonw <braidon@braidonwhatley.com>
2024-03-27 21:29:27 +1100

    wip

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,9 +4,10 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("Iosevka Term", { stretch = "Expanded", weight = "Medium" })
--- config.font = wezterm.font("Berkeley Mono", { weight = "Medium" })
+-- config.font = wezterm.font("Iosevka Term", { stretch = "Expanded", weight = "Regular" })
+config.font = wezterm.font("Berkeley Mono")
 -- config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
+-- config.font = wezterm.font("Fira Code", { weight = "Medium" })
 config.font_size = 14
 config.color_scheme_dirs = { "~/.config/wezterm/colors" }
 config.color_scheme = "Bamboo"
