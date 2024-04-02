c9268e5bc0e1719e4bde297052ae81c1f1129b83
braidonw <braidon@braidonwhatley.com>
2024-04-03 10:13:27 +1100

    wip

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,23 +2,41 @@
 local wezterm = require("wezterm")
 
 -- This will hold the configuration.
-local config = wezterm.config_builder()
+local c = wezterm.config_builder()
 
--- config.font = wezterm.font("Iosevka Term", { stretch = "Expanded", weight = "Regular" })
--- config.font = wezterm.font("Berkeley Mono")
-config.font = wezterm.font("JetBrainsMono Nerd Font")
--- config.font = wezterm.font("Fira Code", { weight = "Medium" })
-config.font_size = 14
-config.line_height = 1.2
-config.color_scheme_dirs = { "~/.config/wezterm/colors" }
-config.color_scheme = "Bamboo"
--- config.color_scheme = "Oxocarbon Dark (Gogh)"
--- config.color_scheme = "Gruvbox dark, hard (base16)"
+-- c.font = wezterm.font("Iosevka Term", { stretch = "Expanded", weight = "Regular" })
+c.font = wezterm.font("Berkeley Mono")
+-- c.font = wezterm.font("JetBrainsMono Nerd Font")
+-- c.font = wezterm.font("Fira Code", { weight = "Medium" })
+-- c.font = wezterm.font("Iosevka Term", { weight = "Regular" })
+c.font_size = 13
+c.line_height = 1.2
+-- c.color_scheme_dirs = { "~/.config/wezterm/colors" }
+-- c.color_scheme = "Bamboo Light"
+-- c.color_scheme = "Bamboo"
+-- c.color_scheme = "Catppuccin Mocha"
+-- c.color_scheme = "Oxocarbon Dark (Gogh)"
+-- c.color_scheme = "Gruvbox dark, hard (base16)"
+c.color_scheme = "GruvboxDarkHard"
 
-config.tab_bar_at_bottom = false
-
-config.window_frame = {
-	font = wezterm.font({ family = "Berkeley Mono", size = 14, weight = "Regular" }),
+local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+custom.background = "#000000"
+custom.tab_bar.background = "#040404"
+custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
+custom.tab_bar.new_tab.bg_color = "#080808"
+c.color_schemes = {
+	["OLEDppuccin"] = custom,
 }
+-- c.color_scheme = "OLEDppuccin"
 
-return config
+c.tab_bar_at_bottom = true
+c.use_fancy_tab_bar = false
+
+c.window_decorations = "RESIZE|INTEGRATED_BUTTONS"
+c.window_padding = { left = 0, right = 0, top = 50, bottom = 0 }
+c.adjust_window_size_when_changing_font_size = false
+c.audible_bell = "Disabled"
+c.default_cursor_style = "BlinkingBar"
+c.inactive_pane_hsb = { brightness = 0.90 }
+
+return c
