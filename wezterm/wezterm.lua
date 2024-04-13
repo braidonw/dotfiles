698a11e3b0f698a77e63aee65f4175ecb6a0776b
braidonw <braidon@braidonwhatley.com>
2024-04-13 22:39:02 +1000

    wip

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,20 +5,22 @@ local wezterm = require("wezterm")
 local c = wezterm.config_builder()
 
 -- c.font = wezterm.font("Iosevka", { stretch = "Expanded", weight = "Regular" })
--- c.font = wezterm.font("Berkeley Mono")
+c.font = wezterm.font("Berkeley Mono")
 -- c.font = wezterm.font("JetBrainsMono Nerd Font")
-c.font = wezterm.font("Fira Code")
+-- c.font = wezterm.font("Fira Code")
 -- c.font = wezterm.font("Iosevka Term", { weight = "Regular" })
 c.font_size = 14
-c.line_height = 1.2
+c.line_height = 1.4
 -- c.color_scheme_dirs = { "~/.config/wezterm/colors" }
 -- c.color_scheme = "Bamboo Light"
+-- c.color_scheme = "Kanagawa (Gogh)"
 -- c.color_scheme = "Bamboo"
--- c.color_scheme = "Catppuccin Mocha"
 -- c.color_scheme = "Oxocarbon Dark (Gogh)"
 -- c.color_scheme = "Gruvbox dark, hard (base16)"
--- c.color_scheme = "GruvboxDarkHard"
-c.color_scheme = "Solarized Dark Higher Contrast (Gogh)"
+c.color_scheme = "GruvboxDarkHard"
+-- c.color_scheme = "Oxocarbon Dark (Gogh)"
+-- -- c.color_scheme = "Solarized Dark Higher Contrast (Gogh)"
+-- c.color_scheme = "Tokyo Night"
 
 local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
 custom.background = "#000000"
@@ -37,7 +39,6 @@ c.use_fancy_tab_bar = false
 c.window_padding = { left = 0, right = 0, top = 10, bottom = 0 }
 c.adjust_window_size_when_changing_font_size = false
 c.audible_bell = "Disabled"
-c.default_cursor_style = "BlinkingBar"
 c.inactive_pane_hsb = { brightness = 0.90 }
 
 return c
